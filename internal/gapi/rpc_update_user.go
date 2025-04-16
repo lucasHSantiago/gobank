32b0668ffefa7c0eb64d306b13ca0f72525107a0
lucasHSantiago <lucashenriquesantiago@gmail.com>
2025-04-15 22:39:40 -0300

    added authorization layer on gRPC

--- internal/gapi/rpc_update_user.go
+++ internal/gapi/rpc_update_user.go
@@ -15,8 +15,17 @@ import (
 )
 
 func (server *Server) UpdateUser(ctx context.Context, req *gen.UpdateUserRequest) (*gen.UpdateUserResponse, error) {
+	authPayload, err := server.authorizeUser(ctx)
+	if err != nil {
+		return nil, unauthenticatedError(err)
+	}
+
 	if violations := validUpdateUserRequest(req); violations != nil {
 		return nil, invalidArgumentError(violations)
+	}
+
+	if authPayload.Username != req.GetUsername() {
+		return nil, status.Errorf(codes.PermissionDenied, "cannot update other user's info")
 	}
 
 	arg := db.UpdateUserParams{
